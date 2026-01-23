74df055998df01325fe550491a1413a9c559646c
SurgeWindAndFire <speedx512@gmail.com>
2026-01-23 00:04:07 -0600

    feat: Firebase hosting setup

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp} from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyATYPFBE432scrEoS5OMaZ-FNPsiBiM-MA",
+    authDomain: "resenity-28203.firebaseapp.com",
+    projectId: "resenity-28203",
+    storageBucket: "resenity-28203.firebasestorage.app",
+    messagingSenderId: "391489603054",
+    appId: "1:391489603054:web:964013d619018a5573faff"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export default app;
